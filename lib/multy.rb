b55730ced782e652da74ccf6b267c4afbb781f90
ganmacs <ganmacs@gmail.com>
2015-05-14 22:26:49 +0900

    prot

--- lib/multy.rb
+++ lib/multy.rb
@@ -1,5 +1,23 @@
 require 'multy/version'
 
 module Multy
-  # Your code goes here...
+  def build_multi_method(name, *args, &block)
+    raise 'invalid arguments number' unless args.size == block.arity
+    name2 = fn_name(*args)
+    meths[name] = {} if meths[name].nil?
+    meths[name][name2] = block
+  end
+
+  def mcall(name, *args)
+    n = args.map(&:class).map(&:to_s).join('_')
+    meths[name][n].call(*args)
+  end
+
+  def fn_name(*args)
+    args.map(&:to_s).join('_')
+  end
+
+  def meths
+    @meths ||= {}
+  end
 end
--- spec/multy_spec.rb
+++ spec/multy_spec.rb
@@ -1,11 +1,18 @@
-require 'spec_helper'
-
 describe Multy do
-  it 'has a version number' do
-    expect(Multy::VERSION).not_to be nil
-  end
+  let(:obj) { Class.new { include Multy }.new }
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'define multi methods' do
+    before do
+      obj.build_multi_method('print', Fixnum) do |a|
+        'fixnum'
+      end
+
+      obj.build_multi_method('print', String) do |a|
+        'string'
+      end
+    end
+
+    it { expect(obj.mcall('print', 1)).to eq 'fixnum' }
+    it { expect(obj.mcall('print', 'a')).to eq 'string' }
   end
 end
